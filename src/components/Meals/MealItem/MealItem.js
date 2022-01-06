5515b0961c8cd5850dda2e7ac38afaf6a8539aaf
Devin Suttler <dsuttler28@gmail.com>
2022-01-06 12:38:46 -0800

    Added the add to cart functionality though logic not finalized

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,23 @@
+import { useContext } from "react";
+
+import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
+
 import classes from './MealItem.module.css'
 
 const MealItem = props => {
+    const cartCtx = useContext(CartContext)
+
     const price = `$${props.price.toFixed(2)}`
+
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+    }
 
     return (
         <li className={classes.meal}>
@@ -9,6 +25,9 @@ const MealItem = props => {
                 <h3>{props.name}</h3>
                 <div className={classes.description}>{props.description}</div>
                 <div className={classes.price}>{price}</div>
+            </div>
+            <div>
+                <MealItemForm onAddToCart={addToCartHandler}/>
             </div>
         </li>
     )
