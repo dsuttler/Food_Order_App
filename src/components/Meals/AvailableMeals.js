cdb7679d60370095e85cda059913aa4bd94d0bac
Devin Suttler <dsuttler28@gmail.com>
2022-01-26 11:58:13 -0800

    Removed static DUMMY_MEALS constant and replaced it with a REST get request to Firebase

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,72 @@
 import Card from "../UI/Card";
 import classes from './AvailableMeals.module.css'
 import MealItem from "./MealItem/MealItem";
+import {useCallback, useEffect, useState} from "react";
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
+// Allows a meal to be pushed to the database
+const putData = async (meals) => {
+
+        const response = await fetch ('https://react-http-4d9db-default-rtdb.firebaseio.com/meals.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meals),
+                headers: {
+                    'Content-type': 'application/json'
+                }
+            }
+        )
+        if(!response.ok) {
+            throw new Error('Something went wrong')
+        }
+}
+
+
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal =>
+    const [mealList, setMealList] = useState([])
+
+
+    // Grab the meals from the database to be shown on the site
+    const getMeals = useCallback(async () => {
+        try {
+            const response = await fetch('https://react-http-4d9db-default-rtdb.firebaseio.com/meals.json')
+
+            if(!response.ok) {
+                throw new Error('Something went wrong')
+            }
+
+            const data = await response.json()
+
+            const tempList = []
+
+            console.log(data)
+
+            for (const key in data) {
+                for(let i = 0; i < data[key].length; i++) {
+                    tempList.push({
+                        id: data[key][i].id,
+                        name: data[key][i].name,
+                        description: data[key][i].description,
+                        price: data[key][i].price
+                    })
+                    console.log(data[key][i].id)
+                    console.log(data[key][i].name)
+                    console.log(data[key][i].description)
+                    console.log(data[key][i].price)
+                }
+            }
+
+            setMealList(tempList)
+
+        } catch (error) {console.log(error.message)}
+
+    }, [])
+
+    useEffect(() => {
+        getMeals()
+    },[getMeals])
+
+    const mealsList = mealList.map(meal =>
         <MealItem
             id={meal.id}
             key={meal.id}
